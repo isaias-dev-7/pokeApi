e009a567c540ed4715368a463e567b6d036b125e
isaias <isaiasosoria7@gmail.com>
2024-12-17 19:53:26 -0500

    websocket-init

--- src/message-ws/message-ws.gateway.ts
+++ src/message-ws/message-ws.gateway.ts
@@ -1,18 +1,26 @@
-import { OnGatewayConnection, OnGatewayDisconnect, WebSocketGateway } from '@nestjs/websockets';
+import { OnGatewayConnection, OnGatewayDisconnect, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
+import { Socket, Server } from 'socket.io';
 import { MessageWsService } from './message-ws.service';
+
 
 @WebSocketGateway({ cors: true})
 export class MessageWsGateway implements  OnGatewayConnection, OnGatewayDisconnect {
+
+  @WebSocketServer() wss: Server;
   
   constructor(
     private readonly messageWsService: MessageWsService
   ) {}
 
-  handleConnection(client: any, ...args: any[]) {
-    
+  handleConnection(client: Socket) {
+    this.messageWsService.registerClient(client);
+    this.wss.emit('clients-updated', this.messageWsService.getConnectedClients());
   }
 
-  handleDisconnect(client: any) {
-    
+  handleDisconnect(client: Socket) {
+    this.messageWsService.removeClient(client);
+    this.wss.emit('clients-updated', this.messageWsService.getConnectedClients());
   }
+
+  
 }
--- src/message-ws/message-ws.service.ts
+++ src/message-ws/message-ws.service.ts
@@ -1,4 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import { Socket } from 'socket.io';
+
+
+
+interface ConnectedClients {
+    [id: string]: Socket,
+}
 
 @Injectable()
-export class MessageWsService {}
+export class MessageWsService {
+
+    private connectedClients: ConnectedClients = {};
+
+    registerClient(client: Socket){
+        this.connectedClients[client.id] = client;
+    }
+
+    removeClient(client: Socket){
+        delete this.connectedClients[client.id];
+    }
+
+    getConnectedClients(): string[] {
+        return Object.keys( this.connectedClients );
+      }
+}
