41e163a6664199bb0cffd40e62abed632134c4e9
isaias <isaiasosoria7@gmail.com>
2024-12-20 07:16:08 -0500

    websocket jwt

--- src/message-ws/message-ws.gateway.ts
+++ src/message-ws/message-ws.gateway.ts
@@ -1,6 +1,9 @@
-import { OnGatewayConnection, OnGatewayDisconnect, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
+import { OnGatewayConnection, OnGatewayDisconnect, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
+import { JwtService } from '@nestjs/jwt';
 import { Socket, Server } from 'socket.io';
 import { MessageWsService } from './message-ws.service';
+import { NewMessageDto } from './dtos/new-message.dto';
+import { JwtPayload } from '../auth/interfaces';
 
 
 @WebSocketGateway({ cors: true})
@@ -9,18 +12,51 @@ export class MessageWsGateway implements  OnGatewayConnection, OnGatewayDisconne
   @WebSocketServer() wss: Server;
   
   constructor(
-    private readonly messageWsService: MessageWsService
+    private readonly messageWsService: MessageWsService,
+
+    private readonly jwtService: JwtService,
   ) {}
 
-  handleConnection(client: Socket) {
-    this.messageWsService.registerClient(client);
+  async handleConnection(client: Socket) {
+    const token = client.handshake.headers.authentication as string;
+    let payload: JwtPayload;
+    try{
+      payload = this.jwtService.verify(token);
+      await this.messageWsService.registerClient(client, payload.id);
+    }catch(error){
+      client.disconnect();
+      return;
+    }
+
     this.wss.emit('clients-updated', this.messageWsService.getConnectedClients());
   }
 
   handleDisconnect(client: Socket) {
     this.messageWsService.removeClient(client);
-    this.wss.emit('clients-updated', this.messageWsService.getConnectedClients());
+    this.wss.emit('clients-updated', this.messageWsService.getConnectedClients());        
   }
 
+  @SubscribeMessage('menssage-from-client')
+  onMessageFromClient(client: Socket, payload: NewMessageDto){
+    
+    //this is to emit an especifique client (param)
+    // client.emit('message-from-server',{
+    //   fullname: "Isaias",
+    //   message: payload.message || "no message",
+    // });
+
+    //this is for emit to all clients with exception client (param)
+    // client.broadcast.emit('message-from-server',{
+    //   fullname: "Isaias",
+    //   message: payload.message || "no message",
+    // });
+
+    //this is for all clients without exception
+    this.wss.emit('message-from-server',{
+       fullname: this.messageWsService.getUserFullName(client.id),
+       message: payload.message || "no message",
+     });
+     
+  }
   
 }
--- src/message-ws/message-ws.service.ts
+++ src/message-ws/message-ws.service.ts
@@ -1,10 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import { Socket } from 'socket.io';
+import { User } from '../auth/entities/user.entity';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
 
 
 
 interface ConnectedClients {
-    [id: string]: Socket,
+    [id: string]: {
+        socket: Socket,
+        user: User,
+    }
 }
 
 @Injectable()
@@ -12,8 +18,19 @@ export class MessageWsService {
 
     private connectedClients: ConnectedClients = {};
 
-    registerClient(client: Socket){
-        this.connectedClients[client.id] = client;
+    constructor(
+        @InjectRepository(User)
+        private readonly userRepository: Repository<User>,
+    ){}
+
+    async registerClient(client: Socket, userId: string){
+        const user = await this.userRepository.findOneBy({ id: userId });
+        if(!user) throw new Error('User not foud');
+        
+        this.connectedClients[client.id] = {
+            socket: client,
+            user,
+        };
     }
 
     removeClient(client: Socket){
@@ -21,6 +38,10 @@ export class MessageWsService {
     }
 
     getConnectedClients(): string[] {
-        return Object.keys( this.connectedClients );
+        return Object.keys( this.connectedClients);
       }
+
+    getUserFullName(socketId: string){
+        return this.connectedClients[socketId].user.fullname;
+    }
 }
